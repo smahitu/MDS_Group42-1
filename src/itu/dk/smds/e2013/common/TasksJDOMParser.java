33e3ffa5b35c9085ec3cdb9dd36816aaa64ade2a
homaxto <spam+github@saxtoft.dk>
2013-09-02 19:36:39 +0200

    Reformating according to java coding standards.

--- src/itu/dk/smds/e2013/common/TasksJDOMParser.java
+++ src/itu/dk/smds/e2013/common/TasksJDOMParser.java
@@ -4,11 +4,6 @@
  */
 package itu.dk.smds.e2013.common;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.JDOMException;
@@ -17,57 +12,41 @@ import org.jdom2.input.SAXBuilder;
 import org.jdom2.xpath.XPathExpression;
 import org.jdom2.xpath.XPathFactory;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
- *
  * @author rao
  */
 public class TasksJDOMParser {
 
-        public static Document GetTasksByQuery(InputStream stream, String query) throws JDOMException, IOException {
-
-
-
+    public static Document GetTasksByQuery(InputStream stream, String query) throws JDOMException, IOException {
         SAXBuilder builder = new SAXBuilder();
 
         //String query = "//task[contains(attendant/@ids,'" + userId + "')]";
 
-        Document doc = null;
+        Document doc;
         try {
-
             doc = builder.build(stream);
         } catch (IOException ex) {
             Logger.getLogger(TasksJDOMParser.class.getName()).log(Level.SEVERE, null, ex);
-
             throw ex;
         }
 
-        
-
         XPathFactory xpfac = XPathFactory.instance();
-
         XPathExpression<Element> xp = xpfac.compile(query, new ElementFilter());
-        
-        
 
-        List<Element> tasks = (List<Element>) xp.evaluate(doc);
-
-        
+        List<Element> tasks = xp.evaluate(doc);
         Document xmlDoc = new Document();
-
         Element root = new Element("tasks");
 
-        for (int index = 0; index < tasks.size(); index++) {
-
-            root.addContent(tasks.get(index).clone());
-
+        for (Element task : tasks) {
+            root.addContent(task.clone());
         }
-
-
-
-
-
         xmlDoc.addContent(root);
-
 
         return xmlDoc;
     }
--- src/itu/dk/smds/e2013/servlets/GetAllTasksServlet.java
+++ src/itu/dk/smds/e2013/servlets/GetAllTasksServlet.java
@@ -5,79 +5,61 @@
 package itu.dk.smds.e2013.servlets;
 
 import itu.dk.smds.e2013.common.TasksJDOMParser;
+import org.jdom2.Document;
+import org.jdom2.JDOMException;
+import org.jdom2.output.XMLOutputter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import org.jdom2.Document;
-import org.jdom2.JDOMException;
-import org.jdom2.output.XMLOutputter;
 
 /**
- *
  * @author rao
  */
 public class GetAllTasksServlet extends HttpServlet {
-
     /**
      * Processes requests for both HTTP
      * <code>GET</code> and
      * <code>POST</code> methods.
      *
-     * @param request servlet request
+     * @param request  servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
+     * @throws IOException      if an I/O error occurs
      */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         try {
-
-
             InputStream xmlStream = getServletContext().getResourceAsStream("/WEB-INF/task-manager-xml.xml");
-
             String query = "//task";
-            
             Document tasksDoc = TasksJDOMParser.GetTasksByQuery(xmlStream, query);
-
-
             response.setContentType("text/Xml;charset=UTF-8");
 
             PrintWriter out = response.getWriter();
-
-
             new XMLOutputter().output(tasksDoc, out);
-
-
-
         } catch (JDOMException ex) {
             Logger.getLogger(GetAllTasksServlet.class.getName()).log(Level.SEVERE, null, ex);
         }
-
-
-
-
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+
     /**
      * Handles the HTTP
      * <code>GET</code> method.
      *
-     * @param request servlet request
+     * @param request  servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
+     * @throws IOException      if an I/O error occurs
      */
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         processRequest(request, response);
     }
 
@@ -85,14 +67,13 @@ public class GetAllTasksServlet extends HttpServlet {
      * Handles the HTTP
      * <code>POST</code> method.
      *
-     * @param request servlet request
+     * @param request  servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
+     * @throws IOException      if an I/O error occurs
      */
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         processRequest(request, response);
     }
 
